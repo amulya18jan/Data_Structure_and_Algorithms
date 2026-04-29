7d048ca1afbb13923d9ea5e2dff514fc68ddcdd8
amulya18jan <amulyaawasth18jan@gmail.com>
2026-04-29 10:08:31 +0530

    Optimized version of Selection sort

---
+++ SelectionSort.java
@@ -0,0 +1,22 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = { 43, 6, 3, 13, 104, 32 };
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+        System.out.print("Sorted Array" + " ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
