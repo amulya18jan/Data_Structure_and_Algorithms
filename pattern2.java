12579fc7af9ba1874d4ad6a4bbfe01c44e7279e5
amulya18jan <amulyaawasth18jan@gmail.com>
2026-04-30 11:50:10 +0530

    solve two pattern problem

--- pattern2.java
+++ pattern2.java
@@ -1,7 +1,7 @@
 public class pattern2 {
     public static void main(String[] args) {
         for(int i =1; i<5; i++){
-            for(int k =5; k>=i;k--){
+            for(int k =5; k<=i;k++){
                 System.out.print(" ");
             }
             for(int j=1;j<=2*i-1;j++){
