eb93f1aa7ebd1d26bdba7bb751e7d73a325eb22e
amulya18jan <amulyaawasth18jan@gmail.com>
2026-04-30 16:53:42 +0530

    sorting from the function

---
+++ arrsort.java
@@ -0,0 +1,12 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class arrsort {
+    public static void main(String[] args) {
+        int arr[] = { 28, 32, 34, 76, 68, 7 };
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+        
+       System.out.println(arr[arr.length-2  ]);
+    }
+}
