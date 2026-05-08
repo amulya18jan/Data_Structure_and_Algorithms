eb26045eacdcb8c10ce2fb51691d4d87fc754647
amulya18jan <amulyaawasth18jan@gmail.com>
2026-05-08 10:18:04 +0530

    Improved Bubblesort

--- BubbleSortImprovement.java
+++ BubbleSortImprovement.java
@@ -1,15 +1,20 @@
 public class BubbleSortImprovement {
     public static void main(String[] args) {
-        int[] arr ={10,3,5,7,8,11,9,4};
+        int[] arr ={298,3,45,1,43,22,44,104};
         int n = arr.length;
     
         for (int i= 0; i<n-1;i++){
+            boolean swapped =false;
             for(int j=0;j<n-i-1;j++){
                 if(arr[j]>arr[j+1]){
                     int temp =arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
+                    swapped =true;
                 }
+            }
+            if(!swapped){
+                break;
             }
             
         }
